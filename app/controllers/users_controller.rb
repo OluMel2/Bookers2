f9ad240f12f63416dadf2394e2e864e832970b52
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-04-23 06:05:21 +0000

    [Update]課題３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
-
+  
+  def index
+    @user = current_user
+  end 
+  
   def edit
     @user = User.find(params[:id])
   end
+  
+  
   def show
+    @user = current_user
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
 end
